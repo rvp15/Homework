b008c82cfad400c51a7485e3c3ff906d4a127aad
rvp15 <vedhagithub@gmail.com>
2022-10-17 00:30:50 -0400

    Completed JeopardyAPI

--- React_Homework/jeopardyapi/src/pages/Main.js
+++ React_Homework/jeopardyapi/src/pages/Main.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 export default function Main ()
 {
 return (
-    <div  className="playbtn" >
-       
+    <div >
+       <h1 className="score txt">Welcome to Jeopardy!</h1>
+       <div  className="playbtn" >
         <Link to='/play'>
             <button className="homebtn">Play</button>
         </Link>
+        </div>
     </div>
 )
 
